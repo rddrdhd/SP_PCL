b4caf369a7faa9210721fe46bdf719f1e96e60cc
rddrdhd <zurkova.leona@email.cz>
2021-11-25 09:00:42 +0100

    shared pointer for get_cloud_from_file()

--- main.cpp
+++ main.cpp
@@ -2,23 +2,29 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 
-int
-main ()
-{
+// Get shared pointer to a cloud from file
+pcl::PointCloud<pcl::PointXYZ>::Ptr get_cloud_from_file(std::string filepath){
+
+    //creates a PointCloud<PointXYZ> boost shared pointer and initializes it.
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
 
-    if (pcl::io::loadPCDFile<pcl::PointXYZ> ("test_pcd.pcd", *cloud) == -1) //* load the file
+    //loads the PointCloud data from file into the binary blob, works for binary/readable exported clouds
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> (filepath, *cloud) == -1)
     {
         PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
-        return (-1);
     }
+    return cloud;
+}
+int main ()
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = get_cloud_from_file("../pcd_files/MODEL_cup_pink.pcd");
     std::cout << "Loaded "
               << cloud->width * cloud->height
               << " data points from test_pcd.pcd with the following fields: "
               << std::endl;
     for (const auto& point: *cloud)
-        std::cout << "    " << point.x
-                  << " "    << point.y
-                  << " "    << point.z << std::endl;
+        std::cout << "\t" << point.x
+                  << "\t" << point.y
+                  << "\t" << point.z << std::endl;
     return (0);
 }
