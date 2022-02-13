85bc6574bc3fce9e78bb12438621c094262f174a
rddrdhd <zurkova.leona@email.cz>
2022-02-13 09:35:51 +0100

    done

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_cloud.h>
 #include <pcl/correspondence.h>
@@ -13,11 +12,8 @@
 #include <pcl/kdtree/impl/kdtree_flann.hpp>
 #include <pcl/common/transforms.h>
 #include <pcl/console/parse.h>
-#include <pcl/point_types.h>
 
-#include "src/loader.h"
-
-typedef pcl::PointXYZ PointType;
+typedef pcl::PointXYZRGBA PointType;
 typedef pcl::Normal NormalType;
 typedef pcl::ReferenceFrame RFType;
 typedef pcl::SHOT352 DescriptorType;
@@ -37,7 +33,94 @@ float descr_rad_ (0.02f);
 float cg_size_ (0.01f);
 float cg_thresh_ (5.0f);
 
+void
+showHelp (char *filename)
+{
+    std::cout << std::endl;
+    std::cout << "***************************************************************************" << std::endl;
+    std::cout << "*                                                                         *" << std::endl;
+    std::cout << "*             Correspondence Grouping Tutorial - Usage Guide              *" << std::endl;
+    std::cout << "*                                                                         *" << std::endl;
+    std::cout << "***************************************************************************" << std::endl << std::endl;
+    std::cout << "Usage: " << filename << " model_filename.pcd scene_filename.pcd [Options]" << std::endl << std::endl;
+    std::cout << "Options:" << std::endl;
+    std::cout << "     -h:                     Show this help." << std::endl;
+    std::cout << "     -k:                     Show used keypoints." << std::endl;
+    std::cout << "     -c:                     Show used correspondences." << std::endl;
+    std::cout << "     -r:                     Compute the model cloud resolution and multiply" << std::endl;
+    std::cout << "                             each radius given by that value." << std::endl;
+    std::cout << "     --algorithm (Hough|GC): Clustering algorithm used (default Hough)." << std::endl;
+    std::cout << "     --model_ss val:         Model uniform sampling radius (default 0.01)" << std::endl;
+    std::cout << "     --scene_ss val:         Scene uniform sampling radius (default 0.03)" << std::endl;
+    std::cout << "     --rf_rad val:           Reference frame radius (default 0.015)" << std::endl;
+    std::cout << "     --descr_rad val:        Descriptor radius (default 0.02)" << std::endl;
+    std::cout << "     --cg_size val:          Cluster size (default 0.01)" << std::endl;
+    std::cout << "     --cg_thresh val:        Clustering threshold (default 5)" << std::endl << std::endl;
+}
 
+void
+parseCommandLine (int argc, char *argv[])
+{
+    //Show help
+    if (pcl::console::find_switch (argc, argv, "-h"))
+    {
+        showHelp (argv[0]);
+        exit (0);
+    }
+
+    //Model & scene filenames
+    std::vector<int> filenames;
+    filenames = pcl::console::parse_file_extension_argument (argc, argv, ".pcd");
+    if (filenames.size () != 2)
+    {
+        std::cout << "Filenames missing.\n";
+        showHelp (argv[0]);
+        exit (-1);
+    }
+
+    model_filename_ = argv[filenames[0]];
+    scene_filename_ = argv[filenames[1]];
+
+    //Program behavior
+    if (pcl::console::find_switch (argc, argv, "-k"))
+    {
+        show_keypoints_ = true;
+    }
+    if (pcl::console::find_switch (argc, argv, "-c"))
+    {
+        show_correspondences_ = true;
+    }
+    if (pcl::console::find_switch (argc, argv, "-r"))
+    {
+        use_cloud_resolution_ = true;
+    }
+
+    std::string used_algorithm;
+    if (pcl::console::parse_argument (argc, argv, "--algorithm", used_algorithm) != -1)
+    {
+        if (used_algorithm.compare ("Hough") == 0)
+        {
+            use_hough_ = true;
+        }else if (used_algorithm.compare ("GC") == 0)
+        {
+            use_hough_ = false;
+        }
+        else
+        {
+            std::cout << "Wrong algorithm name.\n";
+            showHelp (argv[0]);
+            exit (-1);
+        }
+    }
+
+    //General parameters
+    pcl::console::parse_argument (argc, argv, "--model_ss", model_ss_);
+    pcl::console::parse_argument (argc, argv, "--scene_ss", scene_ss_);
+    pcl::console::parse_argument (argc, argv, "--rf_rad", rf_rad_);
+    pcl::console::parse_argument (argc, argv, "--descr_rad", descr_rad_);
+    pcl::console::parse_argument (argc, argv, "--cg_size", cg_size_);
+    pcl::console::parse_argument (argc, argv, "--cg_thresh", cg_thresh_);
+}
 
 double
 computeCloudResolution (const pcl::PointCloud<PointType>::ConstPtr &cloud)
@@ -71,25 +154,10 @@ computeCloudResolution (const pcl::PointCloud<PointType>::ConstPtr &cloud)
     return res;
 }
 
-
-int main ()
+int
+main (int argc, char *argv[])
 {
-    const std::string FOLDER_PATH = "../pcd_files/";
-    const std::string FILE_PATH_MODEL = "MODEL_cup_pink.pcd";
-    const std::string FILE_PATH_SCENE = "SCENE_table_with_mugs.pcd";
-    model_filename_ = FOLDER_PATH + FILE_PATH_MODEL;
-    scene_filename_ = FOLDER_PATH + FILE_PATH_SCENE;
-
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud =
-            get_cloud_from_file(FOLDER_PATH,FILE_PATH_MODEL);
-
-    std::cout << "Loaded " << cloud->width * cloud->height
-              << " data points from test_pcd.pcd with the following fields: " << std::endl;
-    for (const auto& point: *cloud)
-        std::cout << "\t" << point.x
-                  << "\t" << point.y
-                  << "\t" << point.z << std::endl;
-
+    parseCommandLine (argc, argv);
 
     pcl::PointCloud<PointType>::Ptr model (new pcl::PointCloud<PointType> ());
     pcl::PointCloud<PointType>::Ptr model_keypoints (new pcl::PointCloud<PointType> ());
@@ -106,11 +174,13 @@ int main ()
     if (pcl::io::loadPCDFile (model_filename_, *model) < 0)
     {
         std::cout << "Error loading model cloud." << std::endl;
+        showHelp (argv[0]);
         return (-1);
     }
     if (pcl::io::loadPCDFile (scene_filename_, *scene) < 0)
     {
         std::cout << "Error loading scene cloud." << std::endl;
+        showHelp (argv[0]);
         return (-1);
     }
 
